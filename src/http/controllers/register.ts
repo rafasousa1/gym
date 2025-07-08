e477dd9f968e10af83ace5774e004b5cebedcb3a
rafasousa1 <rafasousasilva01@gmail.com>
2025-07-08 12:38:54 -0300

    feat: criando hash de senha e verificando email

--- src/http/controllers/register.ts
+++ src/http/controllers/register.ts
@@ -1,4 +1,5 @@
 import { prisma } from '@/lib/prisma'
+import { hash } from 'bcryptjs'
 import { FastifyRequest, FastifyReply } from 'fastify'
 import { z } from 'zod'
 
@@ -11,11 +12,23 @@ export async function register(req: FastifyRequest, reply: FastifyReply) {
 
     const { name, email, password } = usersBodySchema.parse(req.body)
 
+    const password_hash = await hash(password, 6)
+
+    const userWithSameEmail = await prisma.user.findUnique({
+        where: {
+            email,
+        }
+    })
+
+    if (userWithSameEmail) {
+        return reply.status(409).send()
+    }
+
     await prisma.user.create({
         data: {
             name,
             email,
-            password_hash: password
+            password_hash
         }
     })
 
