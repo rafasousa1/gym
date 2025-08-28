a4b605f58b92781495ccaa661f4b4d72d70bb194
rafasousa1 <rafasousasilva01@gmail.com>
2025-08-28 17:09:25 -0300

    feat: controller do profile

--- src/http/controllers/profile.ts
+++ src/http/controllers/profile.ts
@@ -1,10 +1,16 @@
+import { makeGetUserProfileUseCase } from '@/use-case/factories/make-get-user-profile'
 import { FastifyRequest, FastifyReply } from 'fastify'
 
 
 export async function profile(req: FastifyRequest, reply: FastifyReply) {
-    await req.jwtVerify()
+    const getUserProfile = makeGetUserProfileUseCase()
 
-    console.log(req.user.sub)
+    const { user } = await getUserProfile.execute({
+        userId: req.user.sub
+    })
 
-    return reply.status(200).send()
+    return reply.status(200).send({
+        ...user,
+        password_hash: undefined
+    })
 }
